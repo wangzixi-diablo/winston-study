2fa1d32b8bc111fb85503d35c0aebce8d559f9e4
wangzixi-diablo <cle.ee@163.com>
2021-10-25 15:45:19 +0800

    add number as argument

--- main.js
+++ main.js
@@ -3,12 +3,60 @@ var express = require('express');
 var app = express();
 var DEFAULTPORT = 3003;
 var port = process.env.PORT || DEFAULTPORT;
+
+var args = process.argv.slice(2);
+if( args.length != 1){
+	console.log("USAGE: yarn start 100(number of logs to be output");
+	return;
+}
+
+const NUMBER = args[0];
+
 var winstonTest = require("./winstonTest");
+const expressWinston = require('express-winston');
+const winston = require('winston');
+
+function switchOnExpressWinston(){
+  app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "Jerry Log HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: false, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+  }));
+}
+
+// switchOnExpressWinston();
+
+function testviaNormalLog(){
+  console.time('normal');
+  for( var i = 0; i < NUMBER; i++){
+    console.log("Info: " , i);
+  }
+  console.timeEnd('normal');
+}
+
+function testviaWinstonLog(){
+  console.time('winston');
+  for( var i = 0; i < NUMBER; i++){
+    winstonTest.info(i);
+  }
+  console.timeEnd('winston');
+}
 
 app.get('/', function(_req, res){
    res.send("Hello world");
-   winstonTest.info('I am info');
-   winstonTest.error('I am error');
+   
+   testviaWinstonLog();
+   testviaNormalLog();
+   
 });
 
 app.listen(port, function(){
--- winstonTest.js
+++ winstonTest.js
@@ -2,23 +2,18 @@ const winston = require('winston');
  
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
+  //format: winston.format.json(),
+  // defaultMeta: { service: 'user-service' },
+  /*transports: [
     new winston.transports.File({ filename: 'data/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'data/combined.log' }),
-  ]
+  ]*/
 });
-
 
 if (process.env.NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({
       format: winston.format.combine(
-        winston.format.colorize({ all: true }),
+        // winston.format.colorize({ all: true }),
         winston.format.simple()
     )}));
 }
